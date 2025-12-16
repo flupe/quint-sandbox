2d72aa498f18421f3db6037cd502ae929b699321
flupe <lucas@escot.me>
2025-12-16 11:37:04 +0100

    data-driven tests

--- SimpleBank/tests/bank_mbt.rs
+++ SimpleBank/tests/bank_mbt.rs
@@ -1,4 +1,7 @@
-#[cfg(test)]
+#![feature(custom_test_frameworks)]
+#![test_runner(datatest::runner)]
+
+// #[cfg(test)]
 pub mod tests {
     use itf::de::{self, As};
     use itf::trace_from_str;
@@ -37,27 +40,25 @@ pub mod tests {
     #[derive(Clone, Debug, Deserialize)]
     pub struct State {
         pub bank_state: BankState,
-
         #[serde(with = "As::<de::Option::<_>>")]
         pub error: Option<String>,
-
         #[serde(rename = "mbt::actionTaken")]
         pub action_taken: String,
         #[serde(rename = "mbt::nondetPicks")]
         pub nondet_picks: NondetPicks,
     }
 
-    fn compare_error(trace_error: Option<String>, app_error: Option<String>) {
+    fn compare_error(trace_error: Option<String>, app_error: Result<(), String>) {
         if trace_error.is_some() {
             assert!(
-                app_error.is_some(),
+                app_error.is_err(),
                 "Expected action to fail with error: {:?}, but it succeeded",
                 trace_error
             );
             println!("Action failed as expected");
         } else {
             assert!(
-                app_error.is_none(),
+                app_error.is_ok(),
                 "Expected action to succeed, but it failed with error: {:?}",
                 app_error
             );
@@ -65,66 +66,64 @@ pub mod tests {
         }
     }
 
+    // Data-driven test cases using Quint-exported traces
+    #[datatest::files("traces", { input in r"out(.*)\.itf\.json" })]
     #[test]
-    fn model_test() {
-        for i in 0..10000 {
-            println!("Trace #{}", i);
-            let data = fs::read_to_string(format!("traces/out{}.itf.json", i)).unwrap();
-            let trace: itf::Trace<State> = trace_from_str(data.as_str()).unwrap();
+    fn check_trace(input: &str) {
+        let trace: itf::Trace<State> = trace_from_str(input).unwrap();
 
-            let mut bank_state = trace.states[0].value.bank_state.clone();
+        let mut bank_state = trace.states[0].value.bank_state.clone();
 
-            for state in trace.states {
-                let action_taken = state.value.action_taken;
-                let picks = state.value.nondet_picks;
+        for state in trace.states {
+            let action_taken = state.value.action_taken;
+            let picks        = state.value.nondet_picks;
 
-                match action_taken.as_str() {
-                    "init" => { println!("initializing"); }
+            match action_taken.as_str() {
+                "init" => { println!("initializing"); }
 
-                    "deposit_action" => {
-                        let depositor = picks.depositor.clone().unwrap();
-                        let amount    = picks.amount.clone().unwrap();
-                        println!("deposit({}, {})", depositor, amount);
+                "deposit_action" => {
+                    let depositor = picks.depositor.clone().unwrap();
+                    let amount    = picks.amount.clone().unwrap();
+                    println!("deposit({}, {})", depositor, amount);
 
-                        let res = bank_state.deposit(depositor, amount);
-                        compare_error(state.value.error.clone(), res)
-                    }
-                    "withdraw_action" => {
-                        let withdrawer = picks.withdrawer.clone().unwrap();
-                        let amount     = picks.amount.clone().unwrap();
-                        println!("withdraw({}, {})", withdrawer, amount);
-
-                        let res = bank_state.withdraw(withdrawer, amount);
-                        compare_error(state.value.error.clone(), res)
-                    }
-                    "transfer_action" => {
-                        let sender   = picks.sender.clone().unwrap();
-                        let receiver = picks.receiver.clone().unwrap();
-                        let amount   = picks.amount.clone().unwrap();
-                        println!("transfer({}, {}, {})", sender, receiver, amount);
-
-                        let res = bank_state.transfer(sender, receiver, amount);
-                        compare_error(state.value.error.clone(), res)
-                    }
-                    "buy_investment_action" => {
-                        let buyer  = picks.buyer.clone().unwrap();
-                        let amount = picks.amount.clone().unwrap();
-                        println!("buy_investment({}, {})", buyer, amount);
-
-                        let res = bank_state.buy_investment(buyer, amount);
-                        compare_error(state.value.error.clone(), res)
-                    }
-                    "sell_investment_action" => {
-                        let seller = picks.seller.clone().unwrap();
-                        let id     = picks.id.clone().unwrap();
-                        println!("sell_investment({}, {})", seller, id);
-
-                        let res = bank_state.sell_investment(seller, id);
-                        compare_error(state.value.error.clone(), res)
-                    }
-
-                    action => panic!("Invalid action taken {}", action),
+                    let res = bank_state.deposit(depositor, amount);
+                    compare_error(state.value.error.clone(), res)
                 }
+                "withdraw_action" => {
+                    let withdrawer = picks.withdrawer.clone().unwrap();
+                    let amount     = picks.amount.clone().unwrap();
+                    println!("withdraw({}, {})", withdrawer, amount);
+
+                    let res = bank_state.withdraw(withdrawer, amount);
+                    compare_error(state.value.error.clone(), res)
+                }
+                "transfer_action" => {
+                    let sender   = picks.sender.clone().unwrap();
+                    let receiver = picks.receiver.clone().unwrap();
+                    let amount   = picks.amount.clone().unwrap();
+                    println!("transfer({}, {}, {})", sender, receiver, amount);
+
+                    let res = bank_state.transfer(sender, receiver, amount);
+                    compare_error(state.value.error.clone(), res)
+                }
+                "buy_investment_action" => {
+                    let buyer  = picks.buyer.clone().unwrap();
+                    let amount = picks.amount.clone().unwrap();
+                    println!("buy_investment({}, {})", buyer, amount);
+
+                    let res = bank_state.buy_investment(buyer, amount);
+                    compare_error(state.value.error.clone(), res)
+                }
+                "sell_investment_action" => {
+                    let seller = picks.seller.clone().unwrap();
+                    let id     = picks.id.clone().unwrap();
+                    println!("sell_investment({}, {})", seller, id);
+
+                    let res = bank_state.sell_investment(seller, id);
+                    compare_error(state.value.error.clone(), res)
+                }
+
+                action => panic!("Invalid action taken {}", action),
             }
         }
     }
